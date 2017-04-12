7c6fe2f0fa9281e87e1f1513b0e9f2978d725162
Mahdokht Af <mmafravi@gmail.com>
2017-04-12 16:57:10 -0600

    modules modified?

--- app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
@@ -1,24 +1,14 @@
 package edu.utep.cs.cs4330.battleship;
 
-<<<<<<< HEAD
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Toast;
-=======
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-import java.lang.reflect.Array;
->>>>>>> master
 
 public class MainActivity extends AppCompatActivity {
     private Battleship game;
