e966ad148769a9034ad610b8599bd026d66d35c6
Marco Lopez <marcolp93@gmail.com>
2017-04-13 08:28:21 -0600

    Changed Main activity slightly

--- app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
@@ -1,23 +1,37 @@
 package edu.utep.cs.cs4330.battleship;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private Battleship game;
     Spinner p2p, ai;
+    int conectionType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        conectionType = -1;
 
         game = game.getGame();
         p2p = (Spinner) findViewById(R.id.P2Pspinner);
@@ -41,19 +55,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch ( position ) {
-                    case 0: //name: Play on Network
+                    case 0: //Bluetooth
+//                        startBTGame();
+                        conectionType = 0;
                         break;
-                    case 1: //Bluetooth
-                        //TODO turn on if was off through settings and connect to device
-                        startGame();
+                    case 1: //Wifi Direct
+//                        startWFDirectGame();
+                        conectionType = 1;
                         break;
-                    case 2: //Wifi Direct
-                        //TODO dat extra 10 points doe
-                        startGame();
-                        break;
-                    case 3: //Wifi
-                        //TODO turn on if was off through settings and connect to network
-                        startGame();
+                    case 2: //Wifi
+//                        startWFGame();
+                        conectionType = 2;
                         break;
                 }
             }
@@ -72,15 +84,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch ( position ) {
-                    case 0: //name: Play AI
-                        break;
-                    case 1: //Smart
+                    case 0: //Smart
                         game.addSmartStrategy();
-                        startGame();
+//                        startGame();
                         break;
-                    case 2: //Random
+                    case 1: //Random
                         game.addRandomStrategy();
-                        startGame();
+//                        startGame();
                         break;
                 }
             }
@@ -91,18 +101,106 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /* Starts a game against the AI upon click */
-    public void AIclicked(View v) {
-        toast("plaing AI");
-        Intent intent = new Intent(getApplicationContext(), HumanDeployActivity.class);
-        Log.d("MAIN ACT", "intent made");
+    /* Bluetooth Functionality */
+    private boolean BTenabled() {
+        //checks if BT is on
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if ( btAdapter!=null && btAdapter.isEnabled() )
+            return true;
+        return false;
     }
+    private void turnOnBT() {
+        //creates a window alert: user permission to turn on BT
+        Intent intent = new Intent(BluetoothAdapter.getDefaultAdapter().ACTION_REQUEST_ENABLE);
+        startActivity(intent);
+    }
+    private void startBTGame() {
+        //activity (settings): connects to bluetooth
+        if ( !BTenabled() )
+            turnOnBT();
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        btAdapter.startDiscovery();
+        startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
+        if ( btAdapter.getProfileConnectionState(BluetoothProfile.A2DP)==btAdapter.STATE_CONNECTED )
+            startGame();
+        else createTryAgainDialog("Bluetooth not connected!","Try Again","Cancel");
+    }
+
+    /* Wifi Functionality */
+    private boolean WFenabled() {
+        //checks if WF is on
+        WifiManager m = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        return m.isWifiEnabled();
+    }
+    private void turnOnWF() {
+        //creates a window alert: user permission to turn on WF
+        WifiManager m = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+    }
+    private void startWFGame() {
+        //activity (settings): connects to wifi
+        if ( !WFenabled() )
+            turnOnWF();
+        //fixme it jumps straight to error dialog before turnOnWF()
+        /*
+        ConnectivityManager m = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = m.getNetworkInfo(ConnectivityManager.TYPE_WIFI); //deprecated
+        if ( wifi.isConnected() ) startGame();*/
+        // /*
+        WifiManager m = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifi = m.getConnectionInfo();
+        if ( wifi.getNetworkId()!=-1 ) startGame(); //*/
+        else createTryAgainDialog("Wifi not connected!","Try Again","Cancel");
+    }
+
+    /* Wifi Direct Functionality */
+    private void startWFDirectGame() {
+        //todo extra credit boi
+    }
+
+    /* Creates a dialog for user confirmation */
+    private void createTryAgainDialog(String msg, String positive, String negative) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(msg);
+        alertDialogBuilder.setPositiveButton(positive,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        setP2PSpinner(); //fixme what do when user wants to try connecting again?
+                    }
+                });
+        alertDialogBuilder.setNegativeButton(negative,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+//                        recreate(); //fixme what do when user doesn't want to connect?
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();}
 
     /* Starts the Battleship Game */
     private void startGame() {
-        //TODO set the right activity call boi
         Intent intent = new Intent(getApplicationContext(),HumanDeployActivity.class);
-        //Intent intent = new Intent(getApplicationContext(),GameActivity.class);
         startActivity(intent);
+    }
+
+    public void startAIGame(View view){
+        Intent intent = new Intent(getApplicationContext(),HumanDeployActivity.class);
+        startActivity(intent);
+    }
+
+    public void connectPlayer(View view){
+        switch ( conectionType ) {
+            case 0: //Bluetooth
+                startBTGame();
+                break;
+            case 1: //Wifi Direct
+                startWFDirectGame();
+                break;
+            case 2: //Wifi
+                startWFGame();
+                break;
+        }
     }
 }
